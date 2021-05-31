e1cb233f8d9db8bef3cd16441602dcac2e9889a6
Tristan264 <tristan.bschwenk@icloud.com>
2021-05-31 14:35:48 +0200

    v1

--- app.js
+++ app.js
@@ -7,6 +7,19 @@ document.addEventListener('DOMContentLoaded', () => {
     let slide = 0
     let moving = true;
 
+    let buttons = document.querySelectorAll('button')
+
+    buttons.forEach(el => {
+        el.addEventListener('click', ()=> {
+            console.log("hello");
+        })
+        console.log(el.dataset.sliderIndex);
+    })
+
+    buttons[1].addEventListener('click', () => {
+        console.log("hello");
+    })
+
     initCarousel();
 
     // Add required classes :
@@ -73,38 +86,25 @@ document.addEventListener('DOMContentLoaded', () => {
             // temporarily disable interactivity
             disableInteraction();
             // Update the "old" adjacent slides with "new" ones
-            var newPrevious = slide - 1,
-                newNext = slide + 1,
-                oldPrevious = slide - 2,
+            let newPrevious = slide - 1;
+            let newNext = slide + 1;
+            var oldPrevious = slide - 2,
                 oldNext = slide + 2;
             // Test if carousel has more than three items
-            if ((totalItems - 1) > 3) {
-                // Checks and updates if the new slides are out of bounds
-                if (newPrevious <= 0) {
-                    oldPrevious = (totalItems - 1);
-                } else if (newNext >= (totalItems - 1)) {
-                    oldNext = 0;
-                }
-                // Checks and updates if slide is at the beginning/end
-                if (slide === 0) {
-                    newPrevious = (totalItems - 1);
-                    oldPrevious = (totalItems - 2);
-                    oldNext = (slide + 1);
-                } else if (slide === (totalItems - 1)) {
-                    newPrevious = (slide - 1);
-                    newNext = 0;
-                    oldNext = 1;
-                }
-                // Now we've worked out where we are and where we're going, 
-                // by adding/removing classes we'll trigger the transitions.
-                // Reset old next/prev elements to default classes
-                items[oldPrevious].className = itemClassName;
-                items[oldNext].className = itemClassName;
-                // Add new classes
-                items[newPrevious].classList.add("prev");
-                items[slide].className = itemClassName + " active";
-                items[newNext].className = itemClassName + " next";
-            }
+            // Checks and updates if the new slides are out of bounds
+            if (newPrevious < 0) newPrevious = totalItems - 1;
+            if (newNext >= totalItems) newNext = 0;
+            console.log(newNext, newPrevious, slide);
+            // Now we've worked out where we are and where we're going, 
+            // by adding/removing classes we'll trigger the transitions.
+            // Reset old next/prev elements to default classes
+            document.querySelector('.prev').className = itemClassName;
+            document.querySelector('.next').className = itemClassName;
+            // Add new classes  
+            items[newPrevious].className = itemClassName + " prev";
+            items[slide].className = itemClassName + " active";
+            items[newNext].className = itemClassName + " next";
+           
         }
     }
 
